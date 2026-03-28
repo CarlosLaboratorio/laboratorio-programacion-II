fb6ec946a820999755029ae7994169ddf8a6712d
Carlos Aguirre <carlosanibal815@gmail.com>
2026-03-28 09:44:13 -0300

    primera estructura de la actividad

---
+++ alumnos/santiago_gonzalez/tp1/tp1titulo.py
@@ -0,0 +1,24 @@
+"""
+Sitio oficial de Python: https://www.python.org/
+"""
+'''
+Ejercicio 1:
+1. Escribir las formas de dejar comentarios en Python.
+2. Escribir que el programa imprima el mensaje "Hola Python!" pero que Python este en una variable.
+3. Coloca tu nombre completo en una variable.
+4. Escribir variables con los siguientes tipos de datos: string, int, float, boolean.
+5. Mostrar el tipo de dato de cada variable con el formato print(type(variable)).
+6. Imprimir en consola el mensaje "Hola, mi nombre es [nombre completo], y estoy conociendo Python!" utilizando la variable que contiene tu nombre completo.
+'''
+
+# Actividad 1: Comentarios en Python
+
+# Actividad 2: Imprimir mensaje con variable
+
+# Actividad 3: Variable con nombre completo
+
+# Actividad 4: Variables con diferentes tipos de datos
+
+# Actividad 5: Mostrar tipo de dato de cada variable
+
+# Actividad 6: Imprimir mensaje con nombre completo
