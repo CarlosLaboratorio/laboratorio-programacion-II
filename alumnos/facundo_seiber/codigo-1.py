776f1e3d810236df7191520509b573ff9e47ccd4
Facundo Seiber <davidsseiber@gmail.com>
2026-03-31 22:10:15 -0300

    primer commit

--- alumnos/facundo_seiber/codigo-1.py
+++ alumnos/facundo_seiber/codigo-1.py
@@ -1,1 +1,23 @@
-print("hola hello")
+#  comentario de una sola línea
+"""
+Comentario
+Comentario
+"""
+
+lenguaje = "Python"
+print("Hola " + lenguaje + "!")
+
+nombre_apellido = "Facundo Seiber" 
+
+texto = "hello"     
+entero = 14              
+decimal = 1.5           
+booleano = True         
+
+print(type(texto))
+print(type(entero))
+print(type(decimal))
+print(type(booleano))
+
+print(f"Hola mi nombre es {nombre_apellido},y estoy estudiando python")
+
