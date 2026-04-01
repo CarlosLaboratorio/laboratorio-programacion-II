b4b71de2afb98c29a85885d11a8018b9a72a97c1
Figueroa Leandro <figueroavictor333@gmail.com>
2026-03-31 22:12:05 -0300

    tp1 figueroa

---
+++ alumnos/figueroa_leandro/tp1/python2026.py
@@ -0,0 +1,28 @@
+#comentario de una sola linea
+
+#comentando
+#varias
+#lineas
+
+"""
+laboratorio 2 python 2026
+
+"""
+variable = "python"
+
+NyA = "figueroa leandro"
+
+print (NyA)
+print("hola", variable)
+
+int_num = 26
+cadena_caracteres = "python2026"
+float_num = 50.30
+booleano = True
+
+print ("variable tipo int", type(int_num))
+print ("variable tipo cadena", type(cadena_caracteres))
+print ("variable tipo float", type(float_num))
+print ("variable tipo booleano", type(booleano))
+
+print (f"hola, mi nombre completo es {NyA}, y estoy conociendo python")
