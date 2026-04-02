dc1d3bca286672e5ec8b1da86f2083224215c665
Carlos Plasencia <carlosandres.plasencia@gmail.com>
2026-04-01 22:06:29 -0300

    Carlos TP1

---
+++ alumnos/Plasencia_Carlos/Tp1.py
@@ -0,0 +1,18 @@
+# comentario
+
+
+"""
+comentario multilinea
+"""
+
+nombre = "Plasencia Carlos"
+
+texto = "hola mundo"
+
+entero = 120
+
+decimal = 120.5
+
+print(type(texto), type(entero), type(decimal))
+
+print("Hola, mi nombre " + nombre + ", y estoy conociendo Python!")
