271b8626a66917488520bc25e3567f0c493e4e3c
mimbicaceres <mimbicaceres@gmail.com>
2026-04-07 19:26:43 -0300

    correccion subo ejercicio 1 y 2 - mimbi caceres

---
+++ alumnos/mimbi_caceres/tp1/Tp1.py
@@ -0,0 +1,25 @@
+#  comentario de una linea en Python
+
+'''
+Este es un comentario
+de varias líneas
+'''
+
+
+lenguaje_programacion = "Python"
+
+print("Hola", lenguaje_programacion + "!")
+
+nombre = "Mimbi Caceres"
+saludo = "hola mundo"   
+edad = 22               
+promedio = 8.7          
+aprobado = True         
+
+
+print(type(saludo))
+print(type(edad))
+print(type(promedio))
+print(type(aprobado))
+
+print(f"Hola, mi nombre es {nombre}, y estoy aprendiendo {lenguaje_programacion}!")
---
+++ alumnos/mimbi_caceres/tp1/ejercicio2.py
@@ -0,0 +1,39 @@
+#ejemplo operadores python
+
+# Variables
+a = 10
+b = 5
+c = 20
+
+# Operadores aritméticos
+
+print("Operadores aritméticos")
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("División:", a / b)
+print("Módulo:", a % b)
+print("Potencia:", a ** b)
+print("División entera:", a // b)
+
+
+
+# Operadores de comparación
+print("\nOperadores de comparación")
+
+print("a es igual a b:", a == b)
+print("a es diferente de b:", a != b)
+print("a es mayor que b:", a > b)
+print("a es menor que b:", a < b)
+print("a es mayor o igual que b:", a >= b)
+print("a es menor o igual que b:", a <= b)
+
+
+# Operadores lógicos
+
+print("\nOperadores lógicos")
+
+print("a > b and c > a:", a > b and c > a)
+print("a > b or a > c:", a > b or a > c)
+print("not(a > b):", not(a > b))
