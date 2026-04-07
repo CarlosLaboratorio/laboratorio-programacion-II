030b7e4e7819c957e70922d9a38fde973271261f
anttobocc <anttobocca@gmail.com>
2026-04-07 11:51:51 -0300

    TP1: Ejercicio 1 - Boccalandro Antonella

---
+++ alumnos/Boccalandro_Antonella/tp1/tp.py
@@ -0,0 +1,11 @@
+#comentario
+
+"""comentario"""
+
+nombre = "Boccalandro Antonella"
+texto = "Hola Mundo"
+entero = 8
+decimal = 5.5
+
+print(type (texto), type(entero), type(decimal))
+print("Hola, mi nombre es " + nombre + " y estoy conociendo python")
