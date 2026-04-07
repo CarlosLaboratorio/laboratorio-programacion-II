83b034be3c27e137c138a30ab785190257a38e8c
anttobocc <anttobocca@gmail.com>
2026-04-07 12:45:55 -0300

    Corrijo Ejercicio 1 y agrego Ej 2 - Boccalandro Antonella

--- alumnos/Boccalandro_Antonella/tp1/tp.py
+++ alumnos/Boccalandro_Antonella/tp1/tp.py
@@ -1,11 +1,47 @@
-#comentario
+# ============================
+# EJERCICIO 1
+# ============================
 
+# comentario
 """comentario"""
 
 nombre = "Boccalandro Antonella"
 texto = "Hola Mundo"
 entero = 8
 decimal = 5.5
+lenguaje = "Python"
+booleano = True
 
-print(type (texto), type(entero), type(decimal))
-print("Hola, mi nombre es " + nombre + " y estoy conociendo python")
+print(type(texto))
+print(type(entero))
+print(type(decimal))
+print(type(booleano))
+print(f"Hola {lenguaje}!")
+print(f"Hola, mi nombre es {nombre} y estoy conociendo {lenguaje}!")
+
+# ============================
+# EJERCICIO 2
+# ============================
+
+# ARITMETICOS
+a = 10
+b = 5
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("División:", a / b)
+
+# DE COMPARACION
+print("Igual:", a == b)
+print("Distinto:", a != b)
+print("Mayor:", a > b)
+print("Menor:", a < b)
+
+# LOGICOS
+x = True
+y = False
+
+print("AND:", x and y)
+print("OR:", x or y)
+print("NOT:", not x)
