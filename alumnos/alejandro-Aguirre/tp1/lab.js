658586780fbff9a15009f3e1265fa2fd2a3b2e79
Tu Nombre Completo <127151724+alejepa12@users.noreply.github.com>
2026-04-07 21:31:22 -0300

    Agrego ejercicios simples Python y JavaScript - TP1

---
+++ alumnos/alejandro-Aguirre/tp1/lab.js
@@ -0,0 +1,12 @@
+// TP1 - Laboratorio de Programación II
+// Alumno: Alejandro Aguirre
+
+console.log("=====================================")
+console.log("=====================================");
+console.log("Mi nombre es Alejandro Aguirre");
+console.log("Este es mi primer programa en JavaScript");
+
+// Ejemplo simple
+let numero = 2026;
+console.log("El año actual es: " + numero);
+console.log("2 + 2 = " + (2 + 2));
---
+++ alumnos/alejandro-Aguirre/tp1/lab.py
@@ -0,0 +1,13 @@
+# TP1 - Laboratorio de Programación II
+# Alumno: Alejandro Aguirre
+
+print("=====================================")
+print("hola")
+print("=====================================")
+print("Mi nombre es Alejandro Aguirre")
+print("Este es mi primer programa en Python")
+
+
+numero = 2026
+print(f"El año actual es: {numero}")
+print(f"2 + 2 = {2 + 2}")
