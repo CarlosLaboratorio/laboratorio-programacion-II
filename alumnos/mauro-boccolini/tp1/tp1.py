575aa0092bade59d1d04c9d469c83291e41de2ba
Mauro Boccolini <mauroboccolini.20@gmail.com>
2026-04-07 21:59:06 -0300

    TP1 -Mauro Boccolini

---
+++ alumnos/mauro-boccolini/tp1/tp1.py
@@ -0,0 +1,22 @@
+# comentario simple
+"""
+
+comentario 
+
+multilineas
+"""
+
+nombre = "Mauro Boccolin"
+texto = "Hola Mundo"
+entero = 8
+decimal = 5.5
+lenguaje = "Python"
+booleano = True
+PI = 3.14159
+
+print(type(texto))
+print(type(entero))
+print(type(decimal))
+print(type(booleano))
+print(f"Hola {lenguaje}!")
+print(f"Hola, mi nombre es {nombre} y estoy conociendo {lenguaje}!")
