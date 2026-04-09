117389cb561033cb4f4a591148e109dcb199edd4
Ayala Gonzalo <4yalagonzalo.94@gmail.com>
2026-04-08 22:14:23 -0300

    ejercicio 2

--- alumnos/ayala_gonzalo/tp1.py
+++ alumnos/ayala_gonzalo/tp1.py
@@ -18,3 +18,22 @@ print(type(texto), type(entero), type(decimal))
 
 print("Hola, mi nombre " + nombre + ", y estoy conociendo Python!")
 
+sum = 1 + 1
+
+resta = 2 - 1
+
+multi = 3 * 3
+
+divi = 4 / 2
+
+mayorA = 4 > 2
+
+menorA = 2 < 4
+
+conjuncion = 3 > 2 & 5 > 8
+
+disyuncion = 3 == 3 | 4 % 2 == 0
+
+negacion = not True
+
+ 
