5b7fe367ac7e6651bc33f48d4b6b43c248cffeaf
AMUEDOaaron <aaronamuedo2000@gmail.com>
2026-04-08 22:07:00 -0300

    Mi primer programa

---
+++ alumnos/amuedo-aaron/lab.py
@@ -0,0 +1,10 @@
+
+nombre = input("¿Cómo te llamás? ")
+resultado =0
+numero1 = int(input("Ingresa el primer numero que desea sumar: "))
+numero2= int (input("Ingresa el segundo numero que desea sumar: "))
+print("Hola,", nombre, "👋")
+print("Bienvenido a tu primer programa en Python 🚀")
+resultado = numero1 + numero2
+print(nombre ,"El resultado de la suma es:", resultado)
+ 
