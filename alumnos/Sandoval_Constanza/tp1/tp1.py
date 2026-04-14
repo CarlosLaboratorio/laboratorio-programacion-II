34b986388aca89ca2d56be816c17c2b16aabce5b
cotisanddoval <140985283+cotisanddoval@users.noreply.github.com>
2026-04-14 18:30:39 -0300

    Update tp1.py

--- alumnos/Sandoval_Constanza/tp1/tp1.py
+++ alumnos/Sandoval_Constanza/tp1/tp1.py
@@ -1,1 +1,45 @@
+# Comentarios
 
+# Comentario de una sola línea
+
+"""
+Comentario
+de varias líneas
+"""
+
+'''
+comentario
+multilínea
+'''
+
+
+# Imprimir "Hola Python!" usando una variable
+
+lenguaje = "Python"
+print("Hola " + lenguaje + "!")
+
+
+# Variable con tu nombre completo
+
+nombre_completo = "Constanza Sandoval"
+
+
+# Variables con distintos tipos de datos
+
+texto = "Hola"
+numero_entero = 10
+numero_decimal = 3.14
+es_estudiante = True
+
+
+# Mostrar el tipo de cada variable
+
+print(type(texto))
+print(type(numero_entero))
+print(type(numero_decimal))
+print(type(es_estudiante))
+
+
+# 6. Usar f-string
+
+print(f"Hola, mi nombre es {Constanza_Sandoval}, y estoy conociendo Python!")
