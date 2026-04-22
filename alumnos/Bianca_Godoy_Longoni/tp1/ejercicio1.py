e05c4576d2a28983c82a9002d7deda009f45e87d
Longoni Bianca <longonibianca05@gmail.com>
2026-04-21 21:58:16 -0300

    trabajos de operadores control de flujo y funciones

---
+++ alumnos/Bianca_Godoy_Longoni/tp1/ejercicio1.py
@@ -0,0 +1,25 @@
+# hay distintas formas de hacer comentarios en python
+# con un solo numeral tenemos la forma 1, que es para comentarios de una sola linea
+
+"""
+esta es otra forma
+de hacer comentarios
+en este caso de varias lineas
+"""
+
+python = "python"
+
+print(f"Hola {python}")
+
+nombrecompleto = "Bianca Antonella Godoy Longoni"
+tipostring = "hola mundo"
+tipoint = 8
+tipofloat = 6.7
+tipoboolean = True
+
+print(type(tipostring))
+print(type(tipoint))
+print(type(tipofloat))
+print(type(tipoboolean))
+
+print(f"hola, mi nombre es {nombrecompleto} y estoy conociendo python!")
