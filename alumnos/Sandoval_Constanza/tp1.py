13d58310a04fbefb66ca2e34cab3b10a3918b5c5
coti <costynsando@gmail.com>
2026-05-29 20:21:52 -0300

    Correccion tp1

---
+++ alumnos/Sandoval_Constanza/tp1.py
@@ -0,0 +1,45 @@
+# Comentarios
+
+# Comentario de una sola línea
+
+"""
+Comentario
+de varias líneas
+"""
+
+'''
+comentario
+multilínea
+'''
+
+
+# Imprimir "Hola Python!" usando una variable
+
+lenguaje = "Python"
+print("Hola " + lenguaje + "!")
+
+
+# Variable con tu nombre completo
+
+nombre_completo = "Constanza Sandoval"
+
+
+# Variables con distintos tipos de datos
+
+texto = "Hola"
+numero_entero = 10
+numero_decimal = 3.14
+es_estudiante = True
+
+
+# Mostrar el tipo de cada variable
+
+print(type(texto))
+print(type(numero_entero))
+print(type(numero_decimal))
+print(type(es_estudiante))
+
+
+# 6. Usar f-string
+
+print(f"Hola, mi nombre es {nombre_completo}, y estoy conociendo Python!") # Dentro de llaves va la variable que quiero mostrar, aquí nombre_completo, no Constanza_Sandoval, que es el nombre de la variable. Corregir.
