788a209f59a1f173a3d78016b9e4c6d5a6a81957
Matias Ledesma <matiasledesma362@gmail.com>
2026-06-03 00:24:33 -0300

    Agrega tp1 html y js

---
+++ alumnos/Ledesma_Matias/html/java.js
@@ -0,0 +1,12 @@
+// TP1 - Laboratorio de Programación II
+// Alumno: Matias Ledesma
+
+console.log("=====================================");
+console.log("=====================================");
+console.log("Mi nombre es Matias Ledesma");
+console.log("Este es mi primer programa en JavaScript");
+
+// Ejemplo simple
+let numero = 2026;
+console.log("El año actual es: " + numero);
+console.log("2 + 2 = " + (2 + 2));
