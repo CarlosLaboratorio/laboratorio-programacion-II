b37a8b492bab22b2233412f4363f59acdd092638
JMGaona <juanmariagaona12@gmail.com>
2026-06-09 21:06:06 -0300

    subiendo los codigos de los arvicos de py orientados a objetos

---
+++ alumnos/Gaona_Juan/EjerciciosPyOrientadosObjeto/pooLibro.py
@@ -0,0 +1,12 @@
+class Libro:
+    def __init__(self, titulo, autor, paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+        self.leido = False
+
+libro1 = Libro("Cien años de soledad", "García Márquez", 500)
+print(libro1.titulo) 
+print(libro1.autor) 
+print(libro1.paginas)   
+print(libro1.leido) 
---
+++ alumnos/Gaona_Juan/EjerciciosPyOrientadosObjeto/pooVehiculo.py
@@ -0,0 +1,17 @@
+class Vehiculo:
+    def __init__(self, marca, modelo, año):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+
+class Moto(Vehiculo):
+    def __init__(self, marca, modelo, año, cilindrada, tipo_motor):
+        super().__init__(marca, modelo, año)
+        self.cilindrada = cilindrada
+        self.tipo_motor = tipo_motor
+
+    def __str__(self):
+        return f"Moto: {self.marca} {self.modelo} ({self.año}), {self.cilindrada}cc, motor {self.tipo_motor}"
+
+mi_moto = Moto("Honda", "Wave", 2026, 600, "4T")
+print(mi_moto)
